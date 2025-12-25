081a430582c04a8a74cf9690baed1cdcff40a4dc
Dattatray8 <dattatraymahindrakar@gmail.com>
2025-12-25 19:57:23 +0530

    feat: enhance Hero component with features display and gradient background

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,11 +1,13 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Hero from '../components/home/Hero'
+import Features from '../components/home/Features'
 
 const index = () => {
     return (
         <ScrollView>
             <Hero />
+            <Features />
         </ScrollView>
     )
 }
